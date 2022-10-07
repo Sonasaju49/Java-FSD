020a2fb5072f45fc60f993b4728914cf6f3aa411
Sonasaju49 <112931595+Sonasaju49@users.noreply.github.com>
2022-10-07 12:38:11 +0530

    Add files via upload

---
+++ CRUD/class.js
@@ -0,0 +1,19 @@
+class Person{
+
+    constructor(name)
+
+    {
+
+        this.name=name;
+
+    }
+
+}
+
+const p1 = new  Person('sona');
+
+const p2 = new  Person('chinnu');
+
+console.log(p1.name);
+
+console.log(p2.name);
